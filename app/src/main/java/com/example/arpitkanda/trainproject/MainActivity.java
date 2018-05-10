00debd8f154160b190e4105959c8660703416046
Arpit Kanda <ankitkanda01@gmail.com>
2018-05-10 10:09:01 +0530

    New Update

--- app/src/main/java/com/example/arpitkanda/trainproject/MainActivity.java
+++ app/src/main/java/com/example/arpitkanda/trainproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.arpitkanda.trainproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(2000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(MainActivity.this,
+                            Main2Activity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
     }
 
     @Override
